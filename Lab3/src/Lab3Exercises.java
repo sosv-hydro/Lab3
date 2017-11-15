8a8639cabb40e59e615c2c887a9ddc7e7be0ff04
4015d14 <4015d14@anon.ece.uprm.edu>
2017-11-15 14:23:53 -0400

    Changed frame title

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
+		JFrame myFrame = new JFrame("Sofia Saavedra");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(940, 500);
 		myFrame.setSize(200, 200);
