51103fcb8e31193339a75d6dd897cd2d49987625
4015d14 <4015d14@anon.ece.uprm.edu>
2017-11-15 14:18:46 -0400

    Changed frame size to 200x200 and set location to middle of display

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,9 +3,9 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(940, 500);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
