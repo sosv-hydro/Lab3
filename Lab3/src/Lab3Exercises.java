51daa7474f2bde9eb82528374abf76c898a65c2d
4015d14 <4015d14@anon.ece.uprm.edu>
2017-11-15 14:34:35 -0400

    Created class: changed background color and borders

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -7,5 +7,7 @@ public class Lab3Exercises {
 		myFrame.setLocation(940, 500);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 	}
 }
---
+++ Lab3/src/MyPanelClass.java
@@ -0,0 +1,32 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.JPanel;
+ 
+public class MyPanelClass extends JPanel {
+            /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+			public void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+ 
+                        //Compute interior coordinates
+                        Insets myInsets = this.getInsets();
+                        int x1 = myInsets.left;
+                        int y1 = myInsets.top;
+                        int x2 = getWidth() - myInsets.right - 1;
+                        int y2 = getHeight() - myInsets.bottom - 1;
+                        int width = x2 - x1;
+                        int height = y2 - y1;
+ 
+                        //Paint the background
+                        g.setColor(Color.BLUE);
+                        g.fillRect(x1, y1, width + 1, height + 1);
+                        g.setColor(Color.YELLOW);
+                        g.drawRect(x1, y1, width, height);
+                        g.setColor(Color.WHITE);
+                        g.drawRect(x1+5, y1+5, width-10, height-10);
+            }
+}
